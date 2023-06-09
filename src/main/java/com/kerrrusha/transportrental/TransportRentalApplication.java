98a6025dc41ef10c202c3c851e7bfefcb0b5c148
Kirill Koval <brcyrill2@gmail.com>
2023-06-10 02:40:04 +0300

    Init commit

---
+++ src/main/java/com/kerrrusha/transportrental/TransportRentalApplication.java
@@ -0,0 +1,13 @@
+package com.kerrrusha.transportrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportRentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransportRentalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kerrrusha/transportrental/TransportRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kerrrusha.transportrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportRentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
