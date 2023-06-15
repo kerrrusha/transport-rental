cb589801214906a02534add75fb6621efd6e6532
Kirill Koval <brcyrill2@gmail.com>
2023-06-15 21:57:13 +0300

    fixed bug with non-existent test configurations

--- transport-rental-data/src/test/java/transportrental/TransportRentalDataApplicationTests.java
+++ transport-rental-data/src/test/java/transportrental/TransportRentalDataApplicationTests.java
@@ -1,9 +1,12 @@
 package transportrental;
 
+import com.kerrrusha.transportrental.TransportRentalDataApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = TransportRentalDataApplication.class)
 class TransportRentalDataApplicationTests {
 
     @Test
--- transport-rental-web/src/test/java/com/kerrrusha/transportrental/TransportRentalWebApplicationTests.java
+++ transport-rental-web/src/test/java/com/kerrrusha/transportrental/TransportRentalWebApplicationTests.java
@@ -2,8 +2,10 @@ package com.kerrrusha.transportrental;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = TransportRentalWebApplication.class)
 class TransportRentalWebApplicationTests {
 
     @Test
