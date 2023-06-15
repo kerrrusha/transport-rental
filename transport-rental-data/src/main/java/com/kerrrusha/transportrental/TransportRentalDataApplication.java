3e25295a57cc88786724de5f31df50c912a988a3
Kirill Koval <brcyrill2@gmail.com>
2023-06-15 16:25:31 +0300

    added Main classes

--- transport-rental-data/src/main/java/com/kerrrusha/transportrental/TransportRentalDataApplication.java
+++ transport-rental-data/src/main/java/com/kerrrusha/transportrental/TransportRentalDataApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TransportRentalApplication {
+public class TransportRentalDataApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TransportRentalApplication.class, args);
+        SpringApplication.run(TransportRentalDataApplication.class, args);
     }
 
 }
---
+++ transport-rental-web/src/main/java/com/kerrrusha/transportrental/TransportRentalWebApplication.java
@@ -0,0 +1,13 @@
+package com.kerrrusha.transportrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportRentalWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransportRentalWebApplication.class, args);
+    }
+
+}
