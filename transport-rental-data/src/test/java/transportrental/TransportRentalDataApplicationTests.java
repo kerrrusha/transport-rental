89c573603a0bdfd9f1173482f7da7bf23a20b6b5
Kirill Koval <brcyrill2@gmail.com>
2023-06-15 21:25:26 +0300

    added autoVersionSubmodules=true configuration, added tests

--- transport-rental-data/src/test/java/transportrental/TransportRentalDataApplicationTests.java
+++ transport-rental-data/src/test/java/transportrental/TransportRentalDataApplicationTests.java
@@ -1,10 +1,10 @@
-package com.kerrrusha.transportrental;
+package transportrental;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TransportRentalApplicationTests {
+class TransportRentalDataApplicationTests {
 
     @Test
     void contextLoads() {
---
+++ transport-rental-web/src/test/java/com/kerrrusha/transportrental/TransportRentalWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kerrrusha.transportrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportRentalWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
