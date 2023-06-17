514eb6368f56345ed2bfe7c9860b48c4dde574b4
Kirill Koval <brcyrill2@gmail.com>
2023-06-17 18:34:18 +0300

    improved request mappings for controllers

--- transport-rental-web/src/main/java/com/kerrrusha/transportrental/controller/RentalLogController.java
+++ transport-rental-web/src/main/java/com/kerrrusha/transportrental/controller/RentalLogController.java
@@ -2,11 +2,13 @@ package com.kerrrusha.transportrental.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/log")
 public class RentalLogController {
 
-    @GetMapping({"/log", "/log/index", "/log/index.html"})
+    @GetMapping({"", "/", "/index", "/index.html"})
     public String index() {
         return "log/index";
     }
--- transport-rental-web/src/main/java/com/kerrrusha/transportrental/controller/TransportController.java
+++ transport-rental-web/src/main/java/com/kerrrusha/transportrental/controller/TransportController.java
@@ -2,11 +2,13 @@ package com.kerrrusha.transportrental.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/transport")
 public class TransportController {
 
-    @GetMapping({"/transport", "/transport/index", "/transport/index.html"})
+    @GetMapping({"", "/", "/index", "/index.html"})
     public String index() {
         return "transport/index";
     }
