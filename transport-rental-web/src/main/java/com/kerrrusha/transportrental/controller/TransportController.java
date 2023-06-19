18288c3750f563661026389c704cc3b44a46c65c
Kirill Koval <brcyrill2@gmail.com>
2023-06-20 00:38:34 +0300

    added listing transport on index page, closed #13

--- transport-rental-web/src/main/java/com/kerrrusha/transportrental/controller/TransportController.java
+++ transport-rental-web/src/main/java/com/kerrrusha/transportrental/controller/TransportController.java
@@ -1,15 +1,22 @@
 package com.kerrrusha.transportrental.controller;
 
+import com.kerrrusha.transportrental.service.TransportService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/transport")
+@RequiredArgsConstructor
 public class TransportController {
 
+    private final TransportService transportService;
+
     @GetMapping({"", "/", "/index", "/index.html"})
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("transportList", transportService.findAll());
         return "transport/index";
     }
 
