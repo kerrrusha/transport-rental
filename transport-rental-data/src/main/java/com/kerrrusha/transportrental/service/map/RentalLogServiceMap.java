b14a09b42603758626aaf33791822c116839f2bd
Kirill Koval <brcyrill2@gmail.com>
2023-06-24 22:16:41 +0300

    improved entities saving logic - now saving all the sub-entities to db and assigning their ids

--- transport-rental-data/src/main/java/com/kerrrusha/transportrental/service/map/RentalLogServiceMap.java
+++ transport-rental-data/src/main/java/com/kerrrusha/transportrental/service/map/RentalLogServiceMap.java
@@ -2,19 +2,39 @@ package com.kerrrusha.transportrental.service.map;
 
 import com.kerrrusha.transportrental.model.Customer;
 import com.kerrrusha.transportrental.model.RentalLog;
+import com.kerrrusha.transportrental.model.Transport;
+import com.kerrrusha.transportrental.service.CustomerService;
 import com.kerrrusha.transportrental.service.RentalLogService;
+import com.kerrrusha.transportrental.service.TransportService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import static java.util.Objects.nonNull;
 import static java.util.stream.Collectors.toList;
 
 @Service
+@RequiredArgsConstructor
 public class RentalLogServiceMap extends AbstractMapService<RentalLog, Long> implements RentalLogService {
+
+    private final CustomerService customerService;
+    private final TransportService transportService;
 
     @Override
     public RentalLog save(RentalLog object) {
-        return super.save(object);
+        object = super.save(object);
+
+        if (nonNull(object.getCustomer())) {
+            Customer customerWithId = customerService.save(object.getCustomer());
+            object.setCustomer(customerWithId);
+        }
+        if (nonNull(object.getTransport())) {
+            Transport transportWithId = transportService.save(object.getTransport());
+            object.setTransport(transportWithId);
+        }
+
+        return object;
     }
 
     @Override
--- transport-rental-data/src/main/java/com/kerrrusha/transportrental/service/map/TransportServiceMap.java
+++ transport-rental-data/src/main/java/com/kerrrusha/transportrental/service/map/TransportServiceMap.java
@@ -1,15 +1,30 @@
 package com.kerrrusha.transportrental.service.map;
 
 import com.kerrrusha.transportrental.model.Transport;
+import com.kerrrusha.transportrental.model.TransportType;
 import com.kerrrusha.transportrental.service.TransportService;
+import com.kerrrusha.transportrental.service.TransportTypeService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import static java.util.Objects.nonNull;
+
 @Service
+@RequiredArgsConstructor
 public class TransportServiceMap extends AbstractMapService<Transport, Long> implements TransportService {
+
+    private final TransportTypeService transportTypeService;
 
     @Override
     public Transport save(Transport object) {
-        return super.save(object);
+        object = super.save(object);
+
+        if (nonNull(object.getTransportType())) {
+            TransportType transportTypeWithId = transportTypeService.save(object.getTransportType());
+            object.setTransportType(transportTypeWithId);
+        }
+
+        return object;
     }
 
     @Override
