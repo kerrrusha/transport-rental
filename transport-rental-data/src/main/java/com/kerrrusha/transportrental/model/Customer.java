a07d0f6223155b75203be3bd78f4dc75a813763c
Kirill Koval <brcyrill2@gmail.com>
2023-07-01 02:01:16 +0300

    added constraints

--- transport-rental-data/src/main/java/com/kerrrusha/transportrental/model/Customer.java
+++ transport-rental-data/src/main/java/com/kerrrusha/transportrental/model/Customer.java
@@ -1,6 +1,8 @@
 package com.kerrrusha.transportrental.model;
 
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,6 +11,8 @@ import lombok.Setter;
 @Entity
 public class Customer extends BaseEntity {
 
+    @NotBlank
+    @Size(min = 2, max = 32)
     private String firstName;
     private String lastName;
     private String phone;
